ecc759f2089d92419e5cb45eb374cdd293a27096
william-mcintosh <william-mcintosh@scas.nsw.edu.au>
2026-03-18 22:14:25 +1100

    Created Functions for files
    Moved all logic into functions and created a main() to run these functions. This is the basis for file handling.

--- William_McIntosh_Assessment_1/Recipe_Assessment.py
+++ William_McIntosh_Assessment_1/Recipe_Assessment.py
@@ -13,5 +13,37 @@
 #   print(f"{i}: {n}")
 # 
 
-with open("William_McIntosh_Assessment_1/recipe.txt", "w") as output:
-    output.write("123")
+def create_new_file():
+    try:
+        with open("William_McIntosh_Assessment_1/recipe.txt", "w") as output:
+            output.write("test new")
+    except Exception as e:
+            print("Error:", e)
+
+def modify_existing_file():
+    print("Modifying file...")
+    try:
+        with open("William_McIntosh_Assessment_1/recipe.txt", "w") as output:
+            output.write("\ntest modify")
+    except Exception as e:
+        print("Error:", e)
+
+def get_user_input():
+    return input("New file, Modify, Quit\n").lower()
+
+def main():
+
+    while True:
+        decision = get_user_input()
+
+        if decision in ("", "quit"):
+            break
+        elif decision in ("new file", "new"):
+            create_new_file()
+        elif decision == "modify":
+            modify_existing_file()
+        else:
+            print("Invalid input")
+
+if __name__ =="__main__":
+    main()
