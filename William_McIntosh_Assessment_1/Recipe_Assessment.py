126df4a910c193c4f5c04577a62b0494da074d6b
william-mcintosh <william-mcintosh@scas.nsw.edu.au>
2026-03-18 12:37:31 +1100

    put file

---
+++ William_McIntosh_Assessment_1/Recipe_Assessment.py
@@ -0,0 +1,17 @@
+# ingredients_dictionary = {}
+
+# while True: 
+#   ing = input("Ingredient: ")
+#   if not ing:
+#     break
+#   try:
+#     ingredients_dictionary[ing] = int(input("Quantity: "))
+#   except:
+#     print("Please enter a valid number")
+# print("Total ingredients collected:")
+# for i, n in ingredients_dictionary.items():
+#   print(f"{i}: {n}")
+# 
+
+with open("William_McIntosh_Assessment_1/recipe.txt", "w") as output:
+    output.write("123")
