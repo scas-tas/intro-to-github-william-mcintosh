cd9d403f78c28761b17b10396355222eb2730916
william-mcintosh <william-mcintosh@scas.nsw.edu.au>
2026-05-19 09:38:04 +1000

    1.5.0
    19/05/2026
    Added:
    Interest calculations

--- OOP/Banking.py
+++ OOP/Banking.py
@@ -15,9 +15,21 @@ class Account:
         else:
             self.balance -= amount
             print(f"Withdrew: ${amount}\nNew Balance: ${self.balance}\n")
+    
+    def add_interest(self, interest):
+        time = int(input("\nHow many years: "))
+        print(f"\nOld balance: ${self.balance}")
+        interest_added = self.balance * (1 + interest)**time
+        self.balance += interest_added
+    
+        print(f"Interest added: ${interest_added:.2f}\nNew Balance: ${self.balance:.2f}\n")
+
+
+# --- CONFIG ---
+interest_percentage = 0.05
 
 # --- ACCOUNTS ---
-lliw = Account("123", 50, "lliw")
+lliw = Account("123", 100, "lliw")
 will = Account("6767", 100, "will")
 wili = Account("123456", 1000, "wili")
 
@@ -42,14 +54,14 @@ while True:
         print("Incorrect credentials\n")
     else:
         active = accounts[acc_number]
-        print(f"\nWelcome, {active.owner}! [{acc_number}]\nBalance: ${active.balance}\n")
+        print(f"\nWelcome, {active.owner}! [{acc_number}]\nBalance: ${active.balance:.2f}\n")
 
         # --- CHOICES ---
-        choice = input("Withdraw [a], Deposit [b], Exit [c]\n").lower()
+        choice = input("Withdraw [a], Deposit [b], Add interest [c], Exit [d]\n").lower()
         if choice == "a":
             w_amount = float(input("\nHow much to withdraw?  $"))
             if w_amount <= 0:
-                print("Amount must be positive.")
+                print("Amount must be positive.\n")
             else:
                 active.withdraw(w_amount)
             
@@ -59,8 +71,13 @@ while True:
                 print("Amount must be positive.\n")
             else:
                 active.deposit(d_amount)
+
         elif choice == "c":
+            active.add_interest(interest_percentage)
+
+        elif choice == "d":
             print("\nEnding program...\n")
             exit()
+
         else:
-            print("Invalid")
+            print("Invalid\n")
