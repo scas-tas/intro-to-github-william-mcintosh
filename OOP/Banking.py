9466103a1fa7c0f74ff15f69ed3261dd784f5ec0
william-mcintosh <william-mcintosh@scas.nsw.edu.au>
2026-05-19 19:01:50 +1000

    2.0.0
    Added:
    INTEREST RATES
    decimal formatting
    Crash prevention
    Savings account
    - minimum balance
    - higher interest rate

--- OOP/Banking.py
+++ OOP/Banking.py
@@ -1,4 +1,6 @@
-class Account:
+class BaseAccount:
+    interest_percentage = 0.025
+
     def __init__(self, number, balance, owner):
         self.number = number
         self.balance = balance
@@ -6,35 +8,52 @@ class Account:
     
     def deposit(self, amount):
         self.balance += amount
-        print(f"Deposited: ${amount}\nNew balance: ${self.balance}\n")
+        print(f"Deposited: ${amount:,.2f}\nNew balance: ${self.balance:,.2f}\n")
 
     def withdraw(self, amount):
         if amount > self.balance:
-            print(f"Insufficient Funds.\nTotal balance: ${self.balance}\n")
+            print(f"Insufficient Funds.\nTotal balance: ${self.balance:,.2f}\n")
 
         else:
             self.balance -= amount
-            print(f"Withdrew: ${amount}\nNew Balance: ${self.balance}\n")
+            print(f"Withdrew: ${amount:,.2f}\nNew Balance: ${self.balance:,.2f}\n")
     
     def add_interest(self, interest):
-        time = int(input("\nHow many years: "))
-        print(f"\nOld balance: ${self.balance}")
-        interest_added = self.balance * (1 + interest)**time
-        self.balance += interest_added
-    
-        print(f"Interest added: ${interest_added:.2f}\nNew Balance: ${self.balance:.2f}\n")
+        time = float(input("\nHow many years: "))
+        if time != int(time) or time < 1:
+            print("Must be greater than or equal to 1 year\n")
+            return
+        time = int(time)
+        print(f"\nOld balance: ${self.balance:,.2f}")
+        new_balance = self.balance * (1 + interest)** time
+        interest_added = new_balance - self.balance
+        self.balance = new_balance
+        print(f"Interest added: ${interest_added:,.2f}\nNew Balance: ${self.balance:,.2f}\n")
 
+
+class SavingsAccount(BaseAccount):
+    interest_percentage = 0.1
+    min_balance = 1000
+
+    def add_interest(self, interest):
+        if self.balance < self.min_balance:
+            print(f"\n[][][]\nBalance below ${self.min_balance:,.2f} == Applying base rate ({BaseAccount.interest_percentage * 100:.2f}%)\n[][][]\n")
+            super().add_interest(interest)
+        else:
+            print(f"\nApplying savings rate ({self.interest_percentage * 100:.2f}%)")
+            super().add_interest(self.interest_percentage)
 
 # --- CONFIG ---
-interest_percentage = 0.05
+
 
 # --- ACCOUNTS ---
-lliw = Account("123", 100, "lliw")
-will = Account("6767", 100, "will")
-wili = Account("123456", 1000, "wili")
+lliw = SavingsAccount("123", 10000, "lliw")
+will = BaseAccount("6767", 50, "will")
+wili = BaseAccount("123456", 1000, "wili")
+jayden = SavingsAccount("111", 1000, "jayden")
 
 # --- ACCOUNT DICTIONARY ---
-accounts = {"123":lliw, "6767":will, "123456":wili}
+accounts = {"123":lliw, "6767":will, "123456":wili, "111":jayden}
 
 # --- MAINLINE ---
 first_run = True
@@ -48,32 +67,43 @@ while True:
         if start != "a":
             print("\nEnding program...\n")
             exit()
-
-    acc_number = input("\nEnter account number: ")
+    try:
+        acc_number = input("\nEnter account number: ")
+    except ValueError:
+        print("Invalid input.\n")
+        continue
     if acc_number not in accounts:
-        print("Incorrect credentials\n")
+        print("Invalid credentials\n")
     else:
         active = accounts[acc_number]
-        print(f"\nWelcome, {active.owner}! [{acc_number}]\nBalance: ${active.balance:.2f}\n")
+        print(f"\nWelcome, {active.owner}! [{acc_number}]\nBalance: ${active.balance:,.2f}\n")
 
         # --- CHOICES ---
         choice = input("Withdraw [a], Deposit [b], Add interest [c], Exit [d]\n").lower()
         if choice == "a":
-            w_amount = float(input("\nHow much to withdraw?  $"))
+            try:
+                w_amount = float(input("\nHow much to withdraw?  $"))
+            except ValueError:
+                print("Invalid input.\n")
+                continue
             if w_amount <= 0:
                 print("Amount must be positive.\n")
             else:
                 active.withdraw(w_amount)
             
         elif choice == "b":
-            d_amount = float(input("\nHow much to deposit?  $"))
+            try:
+                d_amount = float(input("\nHow much to deposit?  $"))
+            except ValueError:
+                print("Invalid input.\n")
+                continue
             if d_amount <= 0:
                 print("Amount must be positive.\n")
             else:
                 active.deposit(d_amount)
 
         elif choice == "c":
-            active.add_interest(interest_percentage)
+            active.add_interest(active.interest_percentage)
 
         elif choice == "d":
             print("\nEnding program...\n")
