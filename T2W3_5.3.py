1be0381fe738a006e2c3ecb5058d4108b8528d06
william-mcintosh <william-mcintosh@scas.nsw.edu.au>
2026-05-25 13:31:15 +1000

    Homework for 20/05/26

--- T2W3_5.3.py
+++ T2W3_5.3.py
@@ -1,20 +1,24 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even = [x for x in numbers if x % 2 ==0]
-print(even)
+def numbers():
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even = [x for x in numbers if x % 2 ==0]
+    print(even)
 
-names = ["Alex",
-        "Alexandra",
-        "Ben",
-        "Benjamin",
-        "Cat",
-        "Catherine",
-        "Dan"]
+def names():
+    names = ["Alex",
+            "Alexandra",
+            "Ben",
+            "Benjamin",
+            "Cat",
+            "Catherine",
+            "Dan"]
+    long_names = [name for name in names if len(name) > 5]
+    print(long_names)
 
-long_names = [name for name in names if len(name) > 5]
-print(long_names)
+def sentence():
+    sentence = "I love learning Python"
+    words = sentence.split()
+    reversed_words = [w[::-1] for w in words]
+    result = " ".join(words)
+    print(result)
 
-sentence = "I love learning Python"
-words = sentence.split()
-words[::-1]
-reversed_sentence = " ".join(words)
-print(reversed_sentence)
+numbers(), names(), sentence()
